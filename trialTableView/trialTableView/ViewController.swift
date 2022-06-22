2ec65f367ab48039f44d9da5f34189cff5f5435b
Volkan Alkan <56774809+volkanalkan@users.noreply.github.com>
2022-06-22 17:11:57 +0300

    -
    -

---
+++ trialTableView/trialTableView/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  trialTableView
+//
+//  Created by Volkan Alkan on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Homer Simpson"
+        return cell
+    }
+
+}
+
---
+++ trialTableView/trialTableView/detailsVc.swift
@@ -0,0 +1,22 @@
+//
+//  detailsVc.swift
+//  trialTableView
+//
+//  Created by Volkan Alkan on 22.06.2022.
+//
+
+import UIKit
+
+class detailsVc: UIViewController{
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
